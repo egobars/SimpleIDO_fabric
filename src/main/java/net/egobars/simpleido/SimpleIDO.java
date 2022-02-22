1de005287390f6a6a4bb712661b0d5c4bd18cb0f
Egor Molchan <egobars@yandex.ru>
2022-02-22 17:35:01 +0300

    Some bug fixes

--- src/main/java/net/egobars/simpleido/SimpleIDO.java
+++ src/main/java/net/egobars/simpleido/SimpleIDO.java
@@ -16,31 +16,35 @@ public class SimpleIDO implements ModInitializer {
     @Override
     public void onInitialize() {
         UseBlockCallback.EVENT.register((player, world, hand, hitresult) -> {
-            if (hand == Hand.MAIN_HAND) {
-                BlockPos pos = hitresult.getBlockPos();
-                BlockState state = world.getBlockState(pos);
+            BlockPos pos = hitresult.getBlockPos();
+            BlockState state = world.getBlockState(pos);
+            if (!player.isSneaking() || (player.getMainHandStack().isEmpty() && player.getOffHandStack().isEmpty())) {
                 if (state.getBlock() == Blocks.IRON_DOOR) {
-                    DoorBlock doorBlock = (DoorBlock) state.getBlock();
-                    state = state.cycle(Properties.OPEN);
-                    world.setBlockState(pos, state, 10);
-                    world.syncWorldEvent(player, state.get(Properties.OPEN) ? 1005 : 1011, pos, 0);
-                    world.emitGameEvent(player, doorBlock.isOpen(state) ? GameEvent.BLOCK_OPEN : GameEvent.BLOCK_CLOSE, pos);
-                    player.handSwinging = true;
+                    if (hand == Hand.MAIN_HAND) {
+                        DoorBlock doorBlock = (DoorBlock) state.getBlock();
+                        state = state.cycle(Properties.OPEN);
+                        world.setBlockState(pos, state, 10);
+                        world.syncWorldEvent(player, state.get(Properties.OPEN) ? 1005 : 1011, pos, 0);
+                        world.emitGameEvent(player, doorBlock.isOpen(state) ? GameEvent.BLOCK_OPEN : GameEvent.BLOCK_CLOSE, pos);
+                        player.handSwinging = true;
+                    }
                     return ActionResult.success(world.isClient);
                 } else if (state.getBlock() == Blocks.IRON_TRAPDOOR) {
-                    if (!world.isClient()) {
-                        state = state.cycle(Properties.OPEN);
-                        world.setBlockState(pos, state, Block.NOTIFY_LISTENERS);
-                        if (state.get(Properties.WATERLOGGED)) {
-                            world.createAndScheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+                    if (hand == Hand.MAIN_HAND) {
+                        if (!world.isClient()) {
+                            state = state.cycle(Properties.OPEN);
+                            world.setBlockState(pos, state, Block.NOTIFY_LISTENERS);
+                            if (state.get(Properties.WATERLOGGED)) {
+                                world.createAndScheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+                            }
+                        } else {
+                            boolean is_open = state.get(Properties.OPEN);
+                            if (is_open) world.syncWorldEvent(player, WorldEvents.IRON_TRAPDOOR_OPENS, pos, 0);
+                            else world.syncWorldEvent(player, WorldEvents.IRON_TRAPDOOR_CLOSES, pos, 0);
+                            world.emitGameEvent(player, is_open ? GameEvent.BLOCK_OPEN : GameEvent.BLOCK_CLOSE, pos);
                         }
-                    } else {
-                        boolean is_open = state.get(Properties.OPEN);
-                        if (is_open) world.syncWorldEvent(player, WorldEvents.IRON_TRAPDOOR_OPENS, pos, 0);
-                        else world.syncWorldEvent(player, WorldEvents.IRON_TRAPDOOR_CLOSES, pos, 0);
-                        world.emitGameEvent(player, is_open ? GameEvent.BLOCK_OPEN : GameEvent.BLOCK_CLOSE, pos);
+                        player.handSwinging = true;
                     }
-                    player.handSwinging = true;
                     return ActionResult.success(world.isClient);
                 }
             }
